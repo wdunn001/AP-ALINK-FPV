38d3861a0bba3d3531972f38e860a2759e3bc972
cara <129581427+carabidulebabat@users.noreply.github.com>
2025-06-05 19:05:19 +0200

    Add sta algorithm
    soft tx power algorithm will  increase and decrease tx power depends on the link quality adjust txpower_index in db as you want. 200 is the base value is 2db

--- ap_alink.sh
+++ ap_alink.sh
@@ -7,6 +7,16 @@ bitratemax=40              # Bitrate maximum (en Mbps)
 bitratemin=0               # Bitrate minimum (en Mbps)
 
 fail_count=0               # Compteur de pannes GS
+#auto=True 
+
+#txpowermax=2000 #for the moment is only for stock af1 without custom pa
+txpower_index=200
+
+#soft tx power alg (sta)
+#will increase tx power at few db to get the link rock solid, is different to auto which will increase tx power at the maximum, here is just few db less or few db more around a base value 15db will become at max 17db for exemple and come to 15db after
+#for exemple it will increase of index 200 more and 200 down so max 17db is not exponantial
+
+
 
 # FONCTION : ping le GS et retourne le RTT max
 get_max_rtt() {
@@ -32,6 +42,34 @@ get_max_rtt() {
 get_dbm() {
     iw dev wlan0 station dump 2>/dev/null | grep 'signal:' | awk '{print $2}'
     
+}
+
+get_tx_power() {
+    iw dev wlan0 info 2>/dev/null | grep 'tx power' | awk 'print{3}'
+}
+
+get_dynamic_txpower_interval() {
+    powertx=$(get_tx_power)
+    echo $(awk -v d="$powertx" 'BEGIN {
+        if (d > -40)      print 100;
+        else if (d > -60) print 200;
+        else if (d > -75) print 300;
+        else if (d > -85) print 500;
+        else              print 700;
+    }')
+}
+
+
+
+get_dynamic_txpower_decrease() {
+    powertx=$(get_tx_power)
+    echo $(awk -v d="$powertx" 'BEGIN {
+        if (d > -40)      print 700;
+        else if (d > -60) print 400;
+        else if (d > -75) print 300;
+        else if (d > -85) print 200;
+        else              print 100;
+    }')
 }
 
 
@@ -62,21 +100,28 @@ get_dynamic_decrease() {
     dbm=$(get_dbm)
     echo $(awk -v d="$dbm" 'BEGIN {
         if (d > -60)      print 2;    
-        else if (d > -75) print 3;  
-        else if (d > -85) print 5;    
-        else              print 7;    
+        else if (d > -75) print 4;  
+        else if (d > -85) print 10;    
+        else              print 20;    
     }')
 }
 
-
+txpower_get=$(get_tx_power)
 # BOUCLE PRINCIPALE
 while true; do
     sleep 1
+    
+    #init variable
+
     dbm=$(get_dbm)
     rtt=$(get_max_rtt)
     interval=$(get_dynamic_intervals)
-    bitratemax=$(get_dynamic_max_bitrate)
     decrease=$(get_dynamic_decrease)
+    bitratemax=$(get_dynamic_max_bitrate)
+    #txpower_decrease=$(get_dynamic_txpower_decrease)
+    #txpower_increase=$(get_dynamic_txpower_interval)
+    
+
     if [ $? -ne 0 ]; then
         fail_count=$((fail_count + 1))
         echo "[$fail_count] Le GS $ip_gs est injoignable"
@@ -91,15 +136,18 @@ while true; do
         if [ "$bitrate" -lt "$bitratemin" ]; then
             bitrate=$bitratemin
         fi
-        wget -q "http://localhost/api/v1/set?video0.bitrate=$((bitrate * 1000))"
-        echo "Bitrate réduit à $bitrate Mbps (RTT > 100ms)"
+        curl -q "http://localhost/api/v1/set?video0.bitrate=$((bitrate * 1000))"
+        iw wlan0 set txpower fixed $((txpower_get * 100 + txpower_index))
+        echo "Bitrate réduit à $bitrate Mbps, up txpower"
     else
         if [ "$bitrate" -ge "$bitratemax" ]; then
             echo "Bitrate max dynamique atteint : $bitrate Mbps (limite : $bitratemax Mbps)"
         else
             bitrate=$((bitrate + interval))
+            iw wlan0 set txpower fixed $((txpower_get * 100 - txpower_index))
+
             [ "$bitrate" -gt "$bitratemax" ] && bitrate=$bitratemax
-            wget -q "http://localhost/api/v1/set?video0.bitrate=$((bitrate * 1000))"
+            curl -q "http://localhost/api/v1/set?video0.bitrate=$((bitrate * 1000))"
             echo "Bitrate augmenté à $bitrate Mbps (+$interval Mbps)"
         fi
         
